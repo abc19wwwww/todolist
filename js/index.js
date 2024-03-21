5debac0049436a101b9809d17c11984b7373ceb9
tung <abc19wwwww@gmail.com>
2024-03-21 22:21:14 +0800

    a

--- js/index.js
+++ js/index.js
@@ -19,20 +19,36 @@ const display = document.querySelector("#display");
 let todos = [];
 
 // 處理函式
+// html 模組化
+function html(todo) {
+  const date = todo.time ? todo.time.split("T")[0] : "";
+  const time = todo.time ? todo.time.split("T")[1] : "";
+  const completedStyle = todo.completed ? "text-decoration: line-through;" : "";
+
+  return `<li>
+            <div>
+            <input type="checkbox" data-id="${todo.id}" ${
+    todo.completed ? "checked" : ""
+  }> 
+              <span class="todo-date" style="${completedStyle}">${date}</span>
+              <span class="todo-time" style="${completedStyle}">${time}</span>
+            </div>
+            <div>
+            <span class="todo-text" style="${completedStyle}">${
+    todo.text
+  }</span>
+              <i class="fa-regular fa-circle-xmark" style="color: #b8795a;"></i>
+            </div>
+          </li>`;
+}
+
+// 將事項顯示在頁面上
 const displayTodo = () => {
-  const displayItem = todos.map(
-    (v) =>
-      `<span class="todo-text">${v.text}</span>
-      <span class="todo-time">${v.time}</span>`
-  );
-
-  const str = `<input type="checkbox">
-  <li>${displayItem}</li>
-  <i class="fa-regular fa-circle-xmark" style="color: #b8795a;"></i>`;
-
-  display.innerHTML = `<ul>${str}</ul>`;
+  const displayItem = todos.map(html).join("");
+  display.innerHTML = `<ul>${displayItem}</ul>`;
 };
 
+// 將輸入的事項加入到陣列中變成物件
 const addTodoObj = () => {
   const todosItem = {
     id: +new Date(),
@@ -43,10 +59,28 @@ const addTodoObj = () => {
 
   todos.unshift(todosItem);
   displayTodo();
+  input.value = "";
+};
+
+// 更新 todo 的 completed 属性和显示效果
+const todoCompleted = (id, completed) => {
+  const todoIndex = todos.findIndex((todo) => todo.id === id);
+  if (todoIndex !== -1) {
+    todos[todoIndex].completed = completed;
+    displayTodo();
+  }
 };
 //  ==========
 
 // 事件
 addBtn.addEventListener("click", () => {
   addTodoObj();
+});
+
+display.addEventListener("change", (event) => {
+  if (event.target.matches('input[type="checkbox"]')) {
+    const id = parseInt(event.target.dataset.id);
+    const completed = event.target.checked;
+    todoCompleted(id, completed);
+  }
 });
