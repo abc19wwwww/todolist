cd0ca2ef8960ac3bada597be7d55f1086a95c302
tung <abc19wwwww@gmail.com>
2024-03-22 17:34:12 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 /* todo:
-
-
+在 li 上 bdclick 可以進行 v.text 文字編輯
+1. 在 li 上綁事件
+2. 編輯狀態時，要把 span 轉換成 input
+3. 按下儲存則相反
  ========== */
 
 // 獲取元素
@@ -20,24 +22,24 @@ let todos = [];
 
 // 處理函式
 // html 模組化
-function html(todo) {
-  const date = todo.time ? todo.time.split("T")[0] : "";
-  const time = todo.time ? todo.time.split("T")[1] : "";
-  const completedStyle = todo.completed ? "text-decoration: line-through;" : "";
+function html(v) {
+  const date = v.time ? v.time.split("T")[0] : "";
+  const time = v.time ? v.time.split("T")[1] : "";
+  const completedStyle = v.completed ? "text-decoration: line-through;" : "";
 
   return `<li>
             <div>
-            <input type="checkbox" data-id="${todo.id}" ${
-    todo.completed ? "checked" : ""
+            <input type="checkbox" data-id="${v.id}" ${
+    v.completed ? "checked" : ""
   }> 
               <span class="todo-date" style="${completedStyle}">${date}</span>
               <span class="todo-time" style="${completedStyle}">${time}</span>
             </div>
             <div>
-            <span class="todo-text" style="${completedStyle}">${
-    todo.text
-  }</span>
-              <i class="fa-regular fa-circle-xmark" style="color: #b8795a;"></i>
+            <span class="todo-text" style="${completedStyle}">${v.text}</span>
+              <i class="fa-regular fa-circle-xmark" data-id="${
+                v.id
+              }" style="color: #b8795a;"></i>
             </div>
           </li>`;
 }
@@ -60,9 +62,10 @@ const addTodoObj = () => {
   todos.unshift(todosItem);
   displayTodo();
   input.value = "";
+  time.value = "";
 };
 
-// 更新 todo 的 completed 属性和显示效果
+// 更新 completed
 const todoCompleted = (id, completed) => {
   const todoIndex = todos.findIndex((todo) => todo.id === id);
   if (todoIndex !== -1) {
@@ -70,17 +73,41 @@ const todoCompleted = (id, completed) => {
     displayTodo();
   }
 };
+
+// 刪除
+const deleteTodo = (id) => {
+  todos = todos.filter((todo) => todo.id !== id);
+  displayTodo();
+};
+
 //  ==========
 
-// 事件
+// 事件監聽
+// 按下加號按鈕時
 addBtn.addEventListener("click", () => {
   addTodoObj();
 });
 
-display.addEventListener("change", (event) => {
-  if (event.target.matches('input[type="checkbox"]')) {
-    const id = parseInt(event.target.dataset.id);
-    const completed = event.target.checked;
+// 按下 enter 時
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    addTodoObj();
+  }
+});
+
+// checkbox 改變時
+display.addEventListener("change", (e) => {
+  if (e.target.matches('input[type="checkbox"]')) {
+    const id = parseInt(e.target.dataset.id);
+    const completed = e.target.checked;
     todoCompleted(id, completed);
+  }
+});
+
+// 按下叉叉按鈕時
+display.addEventListener("click", (e) => {
+  if (e.target.matches(".fa-circle-xmark")) {
+    const id = parseInt(e.target.dataset.id);
+    deleteTodo(id);
   }
 });
